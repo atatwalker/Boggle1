2a26d265db219cbf8abfd4bcdebb25a83b2b1c2f
ALice Walker <walker.alice.e@gmail.com>
2017-09-11 19:15:43 +1000

    Completed letter randomisation

--- source/boggle_board.rb
+++ source/boggle_board.rb
@@ -6,7 +6,7 @@ class BoggleBoard
 
   def shake!
     @spaces.map! do | space |
-      space = @alpahbet.shuffle.take(1)
+      space = @alphabet.shuffle.take(1)
     end
   end
 
@@ -14,10 +14,10 @@ class BoggleBoard
   # represented as a string, e.g., when you pass it to puts
   def to_s
     output_string = String.new()
-     output_string = @spaces[0..3].join('') + "\n"
-     output_string = output_string + @spaces[4..7].join('') + "\n"
-     output_string = output_string + @spaces[8..11].join('') + "\n"
-     output_string = output_string + @spaces[12..16].join('') + "\n"
+     output_string = @spaces[0..3].join(' ') + "\n"
+     output_string = output_string + @spaces[4..7].join(' ') + "\n"
+     output_string = output_string + @spaces[8..11].join(' ') + "\n"
+     output_string = output_string + @spaces[12..16].join(' ') + "\n"
      output_string
   end
 end
@@ -27,4 +27,9 @@ puts board
 board.shake!
 puts board
 
+#delete all of to_s
 #add new symbol for alphabet: @alphabet = ('A'..'Z').to_a
+# add map to the shake function...
+#add to_s
+#add at the end: board.shake!
+#                 puts board
