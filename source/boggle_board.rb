99a77e1f948692ad77e54d704452ef8db8b94750
ALice Walker <walker.alice.e@gmail.com>
2017-09-11 20:01:49 +1000

    Simple dice complete

--- source/boggle_board.rb
+++ source/boggle_board.rb
@@ -1,35 +1,87 @@
+DICE = ["AAEEGN",
+		"ELRTTY",
+		"AOOTTW",
+		"ABBJOO",
+		"EHRTVW",
+		"CIMOTU",
+		"DISTTY",
+		"EIOSST",
+		"DELRVY",
+		"ACHOPS",
+		"HIMNQU",
+		"EEINSU",
+		"EEGHNW",
+		"AFFKPS",
+		"HLNNRZ",
+		"DEILRX"]
+
 class BoggleBoard
-  def initialize
-    @spaces = Array.new(16,"_")
-    @alphabet = ('A'..'Z').to_a
+  def initialize(dice)
+  	@spaces = Array.new(16,"_")
+  	@dice = dice
   end
 
   def shake!
-    @spaces.map! do | space |
-      space = @alphabet.shuffle.take(1)
-    end
+  	@spaces.map!{|space| space = @dice.shuffle.pop(1).first.letter }
+
+  	# @spaces.map!{|space| space = @alphabet.shuffle.take(1) }
+
+  # 	new_array = Array.new
+  # 	@spaces.each do | space |
+  # 		new_array << @alphabet.shuffle.take(1)
+  # 	end
+  # 	@spaces = new_array
   end
 
   # Defining to_s on an object controls how the object is
   # represented as a string, e.g., when you pass it to puts
   def to_s
-    output_string = String.new()
-     output_string = @spaces[0..3].join(' ') + "\n"
-     output_string = output_string + @spaces[4..7].join(' ') + "\n"
-     output_string = output_string + @spaces[8..11].join(' ') + "\n"
-     output_string = output_string + @spaces[12..16].join(' ') + "\n"
-     output_string
+  	output_string = String.new()
+
+  	output_string = @spaces[0..3].join('') + "\n"
+  	output_string = output_string + @spaces[4..7].join('') + "\n"
+  	output_string = output_string + @spaces[8..11].join('') + "\n"
+  	output_string = output_string + @spaces[12..16].join('') + "\n"
+
+    output_string
   end
 end
 
-board = BoggleBoard.new
+class BoggleDice
+	def initialize(letters)
+		@letters = letters.split('')
+		@top_side = String.new
+		self.roll
+	end
+
+	def roll
+		@top_side = @letters.shuffle.take(1)
+	end
+
+	def letter
+		@top_side
+	end
+end
+
+dice = DICE.map{|die| BoggleDice.new(die)}
+
+board = BoggleBoard.new(dice)
+
+
 puts board
+puts "Now shuffling your board"
 board.shake!
 puts board
 
-#delete all of to_s
-#add new symbol for alphabet: @alphabet = ('A'..'Z').to_a
-# add map to the shake function...
-#add to_s
-#add at the end: board.shake!
-#                 puts board
+#add new class for BoggleDice
+# @letters
+# @top_side
+# def roll
+# def letter
+# add CONSTANTS
+# new dice variable... DICE.map
+# board = @BoggleBoard.new(dice)
+# In BoggleBoard def initialize(dice)
+# add dice instance variable
+# remove alphabet
+# in shake, change the map to shuffle the dice
